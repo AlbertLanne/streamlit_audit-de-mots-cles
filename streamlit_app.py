07f338e07c43477caf6fa5d4b37f9b9f725a94bb
medkhaleff <khalefmo@hotmail.com>
2023-05-05 17:36:22 +0200

    Script Audit de mots clés

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,27 @@
-import streamlit as st
+import semrush
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-st.title("Hello Khalef!")
-st.write("Bienvenue sur ma première application Streamlit.")
+# Configurer les identifiants d'authentification pour l'API Google Sheets
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+client = gspread.authorize(creds)
+
+# Ouvrir la feuille de calcul Google Sheets
+sheet = client.open('Ma feuille de calcul').sheet1
+
+# Récupérer les informations sur le mot clé à partir de l'API SEMrush
+api_key = 'ma_clé_api_semush'
+domain = 'mon_domaine.com'
+keyword = 'mon_mot_clé'
+result = semrush.DomainOverview(domain, api_key=api_key, display_limit=10)
+data = result['Rankings']['Organic']
+
+# Créer un DataFrame Pandas à partir des données récupérées
+df = pd.DataFrame(data)
+
+# Écrire les données sur la feuille de calcul Google Sheets
+sheet.clear()
+sheet.update([df.columns.values.tolist()] + df.values.tolist())
