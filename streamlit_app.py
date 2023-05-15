2f205b3c946463f8c7c2765746ca926152ad0e81
medkhaleff <khalefmo@hotmail.com>
2023-05-15 15:17:24 +0200

    Code qui marche mais le google sheets générer n'est pas accessible

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,58 @@
 import streamlit as st
+import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os
 
 # Titre de l'application
 st.title("Génération de Google Sheets")
 
 # Renseignez ici les informations de votre fichier de service Google Sheets
-credentials = ServiceAccountCredentials.from_json_keyfile_name("streamlit-386323-6eccb7ac6d66.json", ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets'])
+credentials = ServiceAccountCredentials.from_json_keyfile_name("streamlit-386323-72843b2dc4f8.json", ['https://www.googleapis.com/auth/drive'])
 client = gspread.authorize(credentials)
 
-# Champ de saisie pour le nom du fichier Google Sheets
-sheet_name = st.text_input("Nom du fichier Google Sheets :")
+# Sélection du répertoire contenant les fichiers Excel
+folder_path = "Streamlit/exemple-Export-Semrush/uplix-2023-audit/Current"
 
-# Champ de saisie pour les données à enregistrer
-data_input = st.text_area("Données à enregistrer (une ligne par donnée) :")
-
-# Bouton pour générer le Google Sheets
-if st.button("Générer le Google Sheets"):
-    # Création du nouveau fichier Google Sheets
-    new_sheet = client.create(sheet_name)
-    worksheet = new_sheet.get_worksheet(0)
+# Bouton pour générer le Google Sheet
+if st.button("Générer le Google Sheet"):
+    # Vérifier si le chemin du dossier est valide
+    if not os.path.isdir(folder_path):
+        st.error("Chemin de dossier invalide.")
+        st.stop()
     
-    # Enregistrement des données dans le Google Sheets
-    data = data_input.split('\n')
-    for i, item in enumerate(data):
-        worksheet.update_cell(i+1, 1, item)
-    
-    st.success("Le Google Sheets a été généré avec succès.")
+    # Fonction de génération du tableau de mots-clés
+    def generate_keywords_table(folder_path):
+        dfs = []
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith(".xlsx"):
+                file_path = os.path.join(folder_path, file_name)
+                df = pd.read_excel(file_path)
+                dfs.append(df)
+        
+        if not dfs:
+            st.error("Aucun fichier Excel trouvé dans le dossier.")
+            return None
+        
+        combined_df = pd.concat(dfs)
+        selected_columns = ['Keyword', 'Position', 'URL']
+        keywords_table = combined_df[selected_columns]
 
-# Affichage du lien vers le Google Sheets généré
-if 'new_sheet' in locals():
-    st.write("Lien vers le Google Sheets généré :")
-    st.write(new_sheet.url)
+        return keywords_table
+    
+    keywords_table = generate_keywords_table(folder_path)
+    
+    if keywords_table is not None:
+        # Création du nouveau fichier Google Sheets
+        new_sheet = client.create("Tableau de mots-clés")
+        worksheet = new_sheet.get_worksheet(0)
+
+        # Enregistrement des données dans le Google Sheets
+        data = [keywords_table.columns.tolist()] + keywords_table.values.tolist()
+        worksheet.update(data)
+
+        st.success("Le Google Sheet a été généré avec succès.")
+
+        # Affichage du lien vers le Google Sheet généré
+        st.write("Lien vers le Google Sheet généré :")
+        st.write(new_sheet.url)
